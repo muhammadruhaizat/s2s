b527f5683c8daabe5d70799f68b701facc4765e0
hizalhanis <hizalhanis@gmail.com>
2017-09-12 10:36:02 +0800

    12092017_1035

--- application/views/displaystar.php
+++ application/views/displaystar.php
@@ -1,68 +1,177 @@
 	<?php $this->load->view('header'); ?>
-		<div class="main-container ace-save-state" id="main-container">
-			<script type="text/javascript">
-				
-			</script>
-			
-			<div class="main-content">
-				<div class="main-content-inner">
-					<div class="breadcrumbs ace-save-state" id="breadcrumbs">
-						<ul class="breadcrumb">
-							<li>
-								<i class="ace-icon fa fa-home home-icon"></i>
-								<a href="#">Home</a>
-							</li>
-							<li class="active">Star Rating</li>
-						</ul><!-- /.breadcrumb -->
-					</div>
 
-					<div class="page-content">
-						<div class="page-header">
-							<h1>
-								Dashboard
-								<small>
-									<i class="ace-icon fa fa-angle-double-right"></i>
-									overview &amp; stats
-								</small>
-							</h1>
-						</div><!-- /.page-header -->
 
-						<div id="mapDashboard" style="text-align:center; padding: 20px 10px 50px 10px;height:490px;">
+<div class="main-content-inner">
+	<div class="breadcrumbs ace-save-state" id="breadcrumbs">
+		<ul class="breadcrumb">
+			<li>
+				<i class="ace-icon fa fa-home home-icon"></i>
+				<a href="<?php echo base_url();?>">Home</a>
+			</li>
+			<li class="active">Analysis</li>
+		</ul><!-- /.breadcrumb -->
+	</div>
+  <style type="text/css">
+    
+ header {background: grey;color:white;}
+.tg  {border-collapse:collapse;border-spacing:0;border-width:1px;border-style:solid;margin:0px auto;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg .tg-yw4l{vertical-align:top}
+.tg .tg-9hbo{font-weight:bold;vertical-align:top}
+@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}
 
-							<?php
 
-							$query 	= $this->db->query("SELECT * FROM `senarai_sekolah` LIMIT 1");
-							foreach ($query->result_array() as $row)
-								{
-        							echo $row['NamaSekolah'];
-        							echo $row['PeringkatSekolah'];
-        							echo $row['PPD'];
-								}
+   </style>
 
-								$star_value = $_COOKIE['star'];
+	<div class="page-content">
+		<div class="page-header">
+			<h1>
+				Analysis
+				<small>
+					<i class="ace-icon fa fa-angle-double-right"></i>
+					result &amp; stats
+				</small>
+			</h1>
+		</div><!-- /.page-header -->
 
-								if ($star_value == '5') { ?>
-                					<img src="<?php echo base_url();?>/assets/images/5-stars.jpg" style="width:500px;"/>
-                					<?php
-            					} else if ($star_value == '4') { ?>
-                					<img src="<?php echo base_url();?>/assets/images/4-stars.jpg" style="width:500px;"/>
-                					<?php
-            					} else if ($star_value == '3') { ?>
-                					<img src="<?php echo base_url();?>/assets/images/3-stars.jpg" style="width:500px;"/>
-                					<?php
-            					} else if ($star_value == '2') { ?>
-                					<img src="<?php echo base_url();?>/assets/images/2-stars.jpg" style="width:500px;"/>
-                					<?php
-            					} else if ($star_value == '1') { ?>
-                					<img src="<?php echo base_url();?>/assets/images/1-star.jpg" style="width:500px;"/>
-                					<?php
-            					}
-            					?>
+		<?php
+
+			$query 	= $this->db->query("SELECT * FROM `senarai_sekolah` LIMIT 1");
+			foreach ($query->result_array() as $row)
+				{
+					$nama = $row['NamaSekolah'];
+					$p_s = $row['PeringkatSekolah'];
+					$PPD = $row['PPD'];
+					$kodsek = $row['KodSekolah'];
+					$alamat = $row['AlamatSurat'];
+					$poskod = $row['PoskodSurat'];
+					$bandar = $row['BandarSurat'];
+					$negeri = $row['Negeri'];
+					$notel = $row['NoTelefon'];
+					$nofax = $row['NoFax'];
+				}
+
+			$query = $this->db->query("SELECT * FROM `assessment`");
+
+			$row = $query->row();
+
+				if (isset($row))
+					{
+        				$star_value = $row->star_rate;
+					}
+
+		?>
+	<div id="mapDashboard" style="text-align:center;">
+		<header>
+  			<h1><?php echo $nama; ?></h1>
+		</header>
+
+		<?php
+
+			if ($star_value == '5') { ?>
+				<img src="<?php echo base_url();?>/assets/images/5-stars.jpg" style="width:500px;"/>
+				<?php
+			} else if ($star_value == '4') { ?>
+				<img src="<?php echo base_url();?>/assets/images/4-stars.jpg" style="width:500px;"/>
+				<?php
+			} else if ($star_value == '3') { ?>
+				<img src="<?php echo base_url();?>/assets/images/3-stars.jpg" style="width:500px;"/>
+				<?php
+			} else if ($star_value == '2') { ?>
+				<img src="<?php echo base_url();?>/assets/images/2-stars.jpg" style="width:500px;"/>
+				<?php
+			} else if ($star_value == '1') { ?>
+				<img src="<?php echo base_url();?>/assets/images/1-star.jpg" style="width:500px;"/>
+				<?php
+			}
+			?>
+	</div>
+
+		<div class="space-12"></div>
+		<div class="row">
+			<div class="col-sm-6" style="width:100%;height:100%;">
+				<div class="tabbable">
+					<ul class="nav nav-tabs padding-12 tab-color-blue background-blue" id="myTab">
+						<li class="active">
+							<a data-toggle="tab" href="#statistic">
+								Overview
+							</a>
+						</li>
+						<li>
+							<a data-toggle="tab" href="#dmapx">
+								Safety Assessment
+							</a>
+						</li>
+						<li>
+							<a data-toggle="tab" href="#dmapoverall">
+								Map Display
+							</a>
+						</li>
+					</ul>
+                                 
+					<div class="tab-content">
+						<div id="statistic" class="tab-pane fade in active">
+							
+
+								<table class="tg">
+  								<tr>
+    								<th class="tg-9hbo">Peringkat sekolah</th>
+    								<th class="tg-yw4l"><?php echo $p_s; ?></th>
+  								</tr>
+  								<tr>
+    								<td class="tg-9hbo">PPD</td>
+    								<td class="tg-yw4l"><?php echo $PPD; ?></td>
+  								</tr>
+  								<tr>
+    								<td class="tg-9hbo">Kod sekolah</td>
+    								<td class="tg-yw4l"><?php echo $kodsek; ?></td>
+  								</tr>
+  								<tr>
+    								<td class="tg-9hbo">Alamat sekolah</td>
+    								<td class="tg-yw4l"><?php echo $alamat; ?></td>
+  								</tr>
+  								<tr>
+    								<td class="tg-9hbo">Poskod</td>
+    								<td class="tg-yw4l"><?php echo $poskod; ?></td>
+  								</tr>
+  								<tr>
+								    <td class="tg-9hbo">Bandar</td>
+								    <td class="tg-yw4l"><?php echo $bandar; ?></td>
+							  </tr>
+							  <tr>
+							    	<td class="tg-9hbo">Negeri</td>
+							    	<td class="tg-yw4l"><?php echo $negeri; ?></td>
+							  </tr>
+							  <tr>
+							    	<td class="tg-9hbo">No telefon</td>
+							    	<td class="tg-yw4l"><?php echo $notel; ?></td>
+							  </tr>
+							  <tr>
+							    	<td class="tg-9hbo">No fax</td>
+							    	<td class="tg-yw4l"><?php echo $nofax; ?></td>
+							  </tr>
+							</table>
+
 
 						</div>
+						
+						<div id="dmapx" class="fade">
 
 
-					</div><!-- /.page-content -->
+
+						</div>
+							
+						<div id="dmapoverall" class="fade">
+
+
+							
+							
+						</div>
+						</div>
+					</div>
 				</div>
-			</div><!-- /.main-content -->
-		</div>
+			</div>				
+		</div><!-- /.row -->
+	</div><!-- /.page-content -->
+</div>
